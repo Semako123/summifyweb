014149a4283b43d04d3c1643cd580defda9b5693
semako123 <zosusemenoch@gmail.com>
2023-07-22 22:06:12 +0100

    Adds funtion to extract text data and cleans it up

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-import requests
+import requests, re
+from bs4 import BeautifulSoup
+from transformers import pipeline
 
 url = input("url: ")
 
@@ -6,3 +8,24 @@ def fetch_data(url):
     data = requests.get(url)
     return data.text
 
+
+def extract_data(html_data):
+    res_obj = {}
+    soup = BeautifulSoup(html_data, "html.parser")
+    body_text = soup.body.get_text()
+    pattern = r"\n{2,}"
+    # Use the re.sub() function to replace the matched pattern with a single carriage return
+    body = re.sub(pattern, "\n", body_text)
+    title = soup.title.string
+    res_obj["title"] = title
+    res_obj["body"] = body
+    return res_obj
+
+
+html_data = fetch_data(url)
+data = extract_data(html_data)
+print(data)
+
+
+def summarize(text):
+    pass
